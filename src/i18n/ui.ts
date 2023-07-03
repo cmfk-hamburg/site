c76849952701970598823eaf74cfe36bc2421657
fanxux <fan.x@hotmail.de>
2023-07-03 23:33:27 +0200

    Initial dummy layout and i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,15 @@
+export const languages = {
+    en: 'English',
+    cn: 'Chinese',
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+    },
+    cn: {
+      'nav.home': '首页',
+    },
+  } as const;
