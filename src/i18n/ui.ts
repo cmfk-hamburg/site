b990f1fa24cc399a6c2feeabe51e785cd3a24182
fanxux <fan.x@hotmail.de>
2023-09-01 23:37:49 +0200

    Start some thing

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,6 +1,6 @@
 export const languages = {
-    en: 'English',
-    cn: 'Chinese',
+    en: 'EN',
+    cn: 'CN',
   };
   
   export const defaultLang = 'en';
@@ -8,8 +8,20 @@ export const languages = {
   export const ui = {
     en: {
       'nav.home': 'Home',
+      'nav.callForFilm': 'Call For Film',
+      'nav.about': 'About',
+      'nav.partner': 'Partner',
+      'nav.volunteer': 'Volunteer',
+      'nav.dataPrivacy': 'Data Privacy',
+      'nav.imprint': 'Imprint',
     },
     cn: {
       'nav.home': '首页',
+      'nav.callForFilm': '招募电影',
+      'nav.about': '关于我们',
+      'nav.partner': '合作伙伴',
+      'nav.volunteer': '志愿者',
+      'nav.dataPrivacy': '数据隐私',
+      'nav.imprint': 'Imprint'
     },
   } as const;
